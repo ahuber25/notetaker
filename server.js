3e7aad8bf2824ef94937f6b997232ce4f888dcab
Scotch <horsinaround235@gmail.com>
2022-10-09 16:42:34 -0400

    trying to fix errors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
+app.use('/apiRoute', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
